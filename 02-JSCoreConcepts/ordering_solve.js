b09fb05bfc5573a7f9bc59b12f8c0c1728173ffd
Kateryna Herasymenko <hkasia98@gmail.com>
2020-03-07 21:30:34 +0100

    ordering

---
+++ 02-JSCoreConcepts/ordering_solve.js
@@ -0,0 +1,30 @@
+const reverse = function(arr) {
+    let arrReversed = [];
+    arr.forEach(item => arrReversed.push(item));
+    arrReversed.reverse();
+    return arrReversed;
+}
+
+const sort = function(arr) {
+  let arrSorted = [];
+    arr.forEach(item => arrSorted.push(item));
+    arrSorted.sort();
+    return arrSorted;
+}
+
+const even = function(arr) {
+    let arrEven = [];
+    for (let i=0; i<arr.length; i++){
+      if(arr[i]%2==0){
+        arrEven.push(arr[i]);
+      }
+    }
+    return arrEven;
+}
+
+let numbers = [8,3,2,1,5,4,6];
+//console.log(numbers);
+//console.log(reverse(numbers));
+//console.log(even(numbers));
+//console.log(reverse(even(numbers)));
+console.log(even(sort(numbers)));
