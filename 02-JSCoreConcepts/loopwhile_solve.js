aaa12ebe19d4c03900896938619adf8c258ce17e
Kateryna Herasymenko <hkasia98@gmail.com>
2020-03-07 20:32:49 +0100

    loop

--- 02-JSCoreConcepts/loopwhile_solve.js
+++ 02-JSCoreConcepts/loopwhile_solve.js
@@ -1,12 +1,6 @@
-let a = 2;
 let i = 2;
-while (i <= 6){
-  if (i == 2){
+
+while(i<=14){
     console.log(i);
-  }
-  if (i % 2 == 0){
-     a = a + 4;
-    console.log(a);
-    }
- i++; 
+    i+=4;
 }
