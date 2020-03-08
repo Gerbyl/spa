a059ae92888fabb4d8bac2f69fae4f539d9b59b3
Kateryna Herasymenko <hkasia98@gmail.com>
2020-03-08 05:19:09 +0100

    university

---
+++ 02-JSCoreConcepts/university_solve.js
@@ -0,0 +1,34 @@
+names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
+
+class Students {
+    constructor(names) {
+        this.names = names;
+    }
+
+    startWith(letter){
+        
+        let arr =  this.names.filter(name=>name.substring(0,1) === letter);
+        return new Students(arr);
+    }
+
+    sort() {
+        return new Students(this.names.slice().sort());
+    }
+
+    get() {
+        return this.names;
+    }
+
+    getFirst(n) {
+        return this.names.slice(0,n); 
+    }
+}
+
+students = new Students(names);
+console.log(students.get());
+console.log(students.sort());
+console.log(students.startWith('P'));
+console.log(students.startWith('M').sort());
+console.log(students.sort().getFirst(4));
+console.log(students.startWith('M').sort().getFirst(2));
+console.log(students.get());
